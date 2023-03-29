51a47a7d63fab3cd05f289a01e82fee64c6adc62
Marcus Vinicius de Souza Flores <79017365+marcusxyyz@users.noreply.github.com>
2023-03-29 09:50:20 -0300

    Add files via upload

---
+++ CurrencyConverter.java
@@ -0,0 +1,11 @@
+package util;
+
+public class CurrencyConverter {
+	
+	public static final double IOF = 0.06;
+	
+	public static double amount(double price, double quantity) {
+		return price * quantity * IOF + price * quantity;
+	}
+
+}
